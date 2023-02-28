73ef1eec0f2b41c00d5903a7f4175be5753120d5
dmonzon-2018140 <dmonzon-2018140@kinal.edu.gt>
2023-02-27 22:13:26 -0600

    Añadir controlador buscar y su ruta

--- models/server.js
+++ models/server.js
@@ -9,6 +9,7 @@ class Server {
 
         this.paths = {
             auth: '/api/auth',
+            buscar: '/api/buscar',
             categorias: '/api/categorias',
             productos: '/api/productos',
             usuarios: '/api/usuarios'
@@ -36,6 +37,7 @@ class Server {
 
     routes(){
         this.app.use(this.paths.auth, require('../routes/auth'));
+        this.app.use(this.paths.buscar, require('../routes/buscar'));
         this.app.use(this.paths.categorias, require('../routes/categoria'));
         this.app.use(this.paths.productos, require('../routes/producto'));
         this.app.use(this.paths.usuarios,require('../routes/usuario'));
